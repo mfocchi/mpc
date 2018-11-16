0dc777f7872a0d0b01ae902c3e553700299d83d1
Michele Focchi <michele.focchi@iit.it>
2018-11-16 20:00:28 +0100

    implemented generate footsteps

---
+++ tests/niraj/generateFootSteps.cpp
@@ -0,0 +1,124 @@
+
+
+#include <crawl_planner/MPCPlanner.h>
+#include <dls_controller/support/ConsoleUtility.h>
+#include <stdlib.h>
+#include <crawl_controller/GaitSequencer.h>
+
+using namespace Eigen;
+using namespace std;
+using namespace iit::dog;
+
+
+#define prt(x) std::cout << #x " = \n" << x << "\n" << std::endl;
+
+GaitSequencer myGaitSchedule;
+double stridePhase;
+iit::dog::LegBoolMap swingingFlag = false; //stores the time of the steps
+iit::dog::LegBoolMap gait_swing_status = false;
+bool detected_switch = false;
+double duty_factor = 0.75;
+//init params
+int horizon_size = 200;//10 default for tests
+double Ts = 0.1;
+double cycle_time = 4.0;
+Vector2d userSpeed;
+LegDataMap<MPCPlanner::footState> feetStates;
+LegDataMap<MPCPlanner::footState> footHolds;
+LegDataMap<double> feetValuesX, feetValuesY;
+LegDataMap<double> initial_feet_x;
+LegDataMap<double> initial_feet_y;
+
+int main()
+{
+//get user input
+newline::getInt("horizon_size:", horizon_size, horizon_size);
+MPCPlanner myPlanner(horizon_size,    Ts,    9.81);
+newline::getDouble("cycle time:", cycle_time, cycle_time);
+
+userSpeed(0)=0.15;
+userSpeed(1)=0.0;
+newline::getDouble("userSpeedX:", userSpeed(0), userSpeed(0));
+newline::getDouble("userSpeedY:", userSpeed(1), userSpeed(1));
+//init stuff
+//sequence of legs in the cycle
+myGaitSchedule.setSequence(iit::dog::RF, iit::dog::LH, iit::dog::LF, iit::dog::RH);
+myGaitSchedule.setTaskServoRate(1/Ts);
+//set duty factors for each leg
+myGaitSchedule.setDutyFactor(duty_factor, duty_factor, duty_factor, duty_factor);
+//set offsets
+myGaitSchedule.setOffsetAboutCycleStart(0.0  ,0.25  , 0.5 , 0.75 ); //TODO fix this it does not reset
+//set cycle duration
+myGaitSchedule.setTotalCycleDuration(cycle_time); //cycle time is only for 1 step!
+
+
+//initial feet position
+feetValuesX[LF] = 0.3;
+feetValuesX[RF] = 0.3;
+feetValuesX[LH] = -0.3;
+feetValuesX[RH] = -0.3;
+//init y all the same
+feetValuesY[LF] = 0.2;
+feetValuesY[RF] = -0.2;
+feetValuesY[LH] = 0.2;
+feetValuesY[RH] = -0.2;
+
+for (int leg=0;leg<4;leg++){
+    feetStates[leg].resize(horizon_size);
+    //set always stances
+    feetStates[leg].swing.setConstant(horizon_size,false);
+}
+
+//run
+for (int i = 0; i < horizon_size; i++)
+{
+    myGaitSchedule.updateGaitScheduler(swingingFlag , detected_switch);
+    feetStates[LF].x[i] = feetValuesX[LF];
+    feetStates[RF].x[i] = feetValuesX[RF];
+    feetStates[LH].x[i] = feetValuesX[LH];
+    feetStates[RH].x[i] = feetValuesX[RH];
+
+    feetStates[LF].y[i] = feetValuesY[LF];
+    feetStates[RF].y[i] = feetValuesY[RF];
+    feetStates[LH].y[i] = feetValuesY[LH];
+    feetStates[RH].y[i] = feetValuesY[RH];
+
+    feetStates[LF].swing[i] = swingingFlag[LF];
+    feetStates[RF].swing[i] = swingingFlag[RF];
+    feetStates[LH].swing[i] = swingingFlag[LH];
+    feetStates[RH].swing[i] = swingingFlag[RH];
+    //advance step
+//    if (detected_switch)
+//    {
+//        for (int leg=LF; leg<=RH;leg++)
+//        {
+//            if (swingingFlag[leg])
+//            {
+//                feetValuesX[leg] += userSpeed(0);
+//                feetValuesY[leg] += userSpeed(1);
+//            }
+//        }
+//        detected_switch = false;
+//    }
+
+    stridePhase = myGaitSchedule.getStrideParametrization(); //this is for debug
+    gait_swing_status = myGaitSchedule.getSwingLegState();
+}
+//myPlanner.computeSteps(userSpeed, initial_feet_x, initial_feet_y, number_of_steps, horizon_size, feetStates, footHolds, A, b, myPlanner,iit::dog::LF);
+
+myPlanner.saveTraj("footPosLF.txt",  feetStates[LF].x, feetStates[LF].y);
+myPlanner.saveTraj("footPosRF.txt",  feetStates[RF].x, feetStates[RF].y);
+myPlanner.saveTraj("footPosLH.txt",  feetStates[LH].x, feetStates[LH].y);
+myPlanner.saveTraj("footPosRH.txt",  feetStates[RH].x, feetStates[RH].y);
+
+
+myPlanner.saveTraj("swingLF.txt",  feetStates[LF].swing);
+myPlanner.saveTraj("swingRF.txt",  feetStates[RF].swing);
+myPlanner.saveTraj("swingLH.txt",  feetStates[LH].swing);
+myPlanner.saveTraj("swingRH.txt",  feetStates[RH].swing);
+
+}
+
+
+
+
---
+++ tests/niraj/plotGait.m
@@ -0,0 +1,89 @@
+close all
+clear all
+clc
+format short g
+format_input = '%f  %f';
+format_input2 = '%f  %f %f';
+
+    
+[time,   footPos_x(1,:),footPos_y(1,:)] = textread('footPosLF.txt', format_input2);
+[time,   footPos_x(2,:),footPos_y(2,:)] = textread('footPosRF.txt', format_input2);
+[time,   footPos_x(3,:),footPos_y(3,:)] = textread('footPosLH.txt', format_input2);
+[time,   footPos_x(4,:),footPos_y(4,:)] = textread('footPosRH.txt', format_input2);
+
+swing=[];
+[time,swing(1,:)] = textread('swingLF.txt', format_input);
+[time,swing(2,:)] = textread('swingRF.txt', format_input);
+[time,swing(3,:)] = textread('swingLH.txt', format_input);
+[time,swing(4,:)] = textread('swingRH.txt', format_input);
+
+
+%%
+addpath('../')
+
+%%
+%STATIC PLOT
+figure 
+plot(time  , swing(1,:),'-bo' );hold on;grid on
+plot(time  , swing(2,:),'-ro' )
+plot(time  , swing(3,:),'-ko' )
+plot(time  , swing(4,:),'-mo' )
+legend('LF','RF','LH','RH')
+
+%%
+
+figure 
+plot(time  , footPos_x(1,:),'-bo' );hold on;grid on
+plot(time  , footPos_x(2,:),'-ro' )
+plot(time  , footPos_x(3,:),'-ko' )
+plot(time  , footPos_x(4,:),'-mo' )
+legend('LF','RF','LH','RH')
+
+
+
+%%
+close all
+
+simfig = figure;
+
+
+for i=1:length(time)
+    stance_vec = {}; 
+    stance_count = 0;
+    for leg=1:4
+        if ~swing(leg,i)
+            stance_count = stance_count +1;
+            stance_vec{ stance_count} = [footPos_x(leg,i); footPos_y(leg,i);0];
+        end
+    end
+    
+    
+    subplot(2,1,1)
+    h = plotPolygon(stance_vec);   
+    hold on
+    xlim([-1 ,2])
+    ylim([-1.1 ,2.0])
+            
+     subplot(2,1,2)
+     plot(time(i), swing(1,i),'.b','MarkerSize',20); hold on;grid on;
+     plot(time(i), swing(2,i),'.r','MarkerSize',20); hold on;grid on;
+     plot(time(i), swing(3,i),'.k','MarkerSize',20); hold on;grid on;
+     plot(time(i), swing(4,i),'.m','MarkerSize',20); hold on;grid on;
+     xlim([0, time(end)])
+     ylim([0,1.1])
+     ylabel('swing leg')
+     legend('LF','RF','LH','RH')
+     
+     
+    pause(0.01+ 1/time)
+    drawnow  
+
+    delete(h); %this deletes the handle
+
+                  
+end   
+   
+if VIDEO
+    aviobj = close(aviobj)
+end
+
